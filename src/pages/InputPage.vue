52042356062f66f1f34ef2635dbcbd362e4598fd
sirinet394 <141398718+sirinet394@users.noreply.github.com>
2023-10-11 21:27:36 +0700

    Update InputPage.vue

--- src/pages/InputPage.vue
+++ src/pages/InputPage.vue
@@ -14,7 +14,7 @@ export default {
   name: 'InputPage',
   data() {
     return {
-      title: 'aphiched tepnanta',
+      title: 'sirinet lianchop',
       firstName: '',
       secondName: '',
     }
